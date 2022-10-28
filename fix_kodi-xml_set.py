a8b66028ecbd8a609e6b13915660c8d7585b1266
Joakim Sandström <joakim.s@webbkontakt.net>
2022-10-28 23:26:12 +0200

    Renamed fix_adv_set.py
    to just explain more what it does

---
+++ fix_kodi-xml_set.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# fix_kodi-xml_set.py destination.xml custom.xml
+
+import sys
+from xml.etree import ElementTree
+
+def run(files):
+    first = None
+    for filename in files:
+        data = ElementTree.parse(filename).getroot()
+        if first is None:
+            first = data
+        else:
+            first.extend(data)
+    if first is not None:
+        print ElementTree.tostring(first)
+
+if __name__ == "__main__":
+    run(sys.argv[1:])
+
